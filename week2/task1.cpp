cd486c8b4c0c26feddc7b991626171c4c4725d9e
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-09 11:11:21 +0700

    change to const char*

--- week2/task1.cpp
+++ week2/task1.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
-#include<string>
-int main(int argc, char* argv[]){
+int main(int argc, const char* argv[]){
     if (argc != 2) { // check if users insert 1 input only
         std::cerr << "There must be only 1 input arguments" << "\n";
         return -1;
     }
 
-    char* input = argv[1];
+    const char* input = argv[1];
 
     int len = std::string(input).length();
 
@@ -16,23 +15,23 @@ int main(int argc, char* argv[]){
     } 
     if (input[0] != '0') { // check if the first character is '0'
         std::cerr << "Hexadecimal must start with 0" << "\n";
-        return -1;
+        return -2;
     } 
 
     if (tolower(input[1]) != 'x') { // check if the second character is 'x' lowercase or uppercase
         std::cerr << "Hexadecimal must have character 'x' uppercase of lowercase at second character" << "\n";
-        return -1;
+        return -3;
     }
 
     for (int i = 2; i < len; i++) {
         if (!isxdigit(input[i])) { // check if the digit is valid in hexadecimal
             std::cerr << "Invalid hexadecimal value" << "\n";
-            return -1;
+            return -4;
         }
 
         if  (!isdigit(input[i]) && !isupper(input[i])) { // check if hexadecimal digits except integers are uppercase only
             std::cerr << "Hexadecimal digits can only contain uppercase characters" << "\n";
-            return -1;
+            return -5;
         }
     }
 
