f605778929695720f8b312b6894798f6b5eed5ae
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-14 10:39:57 +0700

    change code

--- week2/task4.cpp
+++ week2/task4.cpp
@@ -4,19 +4,18 @@
 
 int main(){
     // a) print 200 with and without positive + sign
-    std::cout << 200 << "\n";
-    std::cout << std::setiosflags(std::ios::showpos) << 200 << "\n\n";
-    std::cout.unsetf(std::ios::showpos);
+    std::cout << std::showpos << 200 << "\n";
+    std::cout << std::noshowpos << 200 << "\n\n";
 
     // b) print 10 and 200 left justified with 5-character width
-    std::cout << std::setw(5) << std::setiosflags(std::ios::left) << 10 << "\n";
-    std::cout << 200 << "\n\n";
-    std::cout.unsetf(std::ios::left);
+    std::cout << std::setw(5) << std::left << 10
+                              << 200 << "*\n\n"; // * is used to mark the spaces
 
     char hexachar[6] = {'A', 'B', 'C', 'E', 'F'};
     int hexavalue[6] = {10, 11, 12, 13, 14, 15};
 
     // c) convert hexadecimal to decimal
+    /*
     std::string input;
     std::cout << "Input: ";
     std::cin >> input;
@@ -39,8 +38,14 @@ int main(){
     }
 
     std::cout << "Output: " << decimal << "\n\n";
+    */
+    int num;
+    std::cout << "Enter hexadecimal number: ";
+    std::cin >> std::hex >> num;
+    std::cout << num << "\n\n";
 
     // d) convert 100 to hexadecimal form preceded by 0x
+    /*
     int integer = 100;
     std::string hexa = "";
     int hexaval = 0;
@@ -64,6 +69,9 @@ int main(){
     hexa = "0x" + hexa;
 
     std::cout << "Hexadecimal of 100 is: " << hexa << "\n\n";
+    */
+
+    std::cout << std::hex << std::showbase << 100 << "\n\n";
 
     // e) Print 1.234 with 9-character width, padded by zeros
     std::cout << std::setw(9) << std::setfill('0') << 1.234 << "\n\n";
@@ -73,13 +81,17 @@ int main(){
 
 
     // g) Read a string from the console input, store the string in character array str. Then print out its all characters, separated by a space
-    char str[30];
+    char str[100];
     std::cout << "Enter a string: ";
-    std::cin.ignore();
-    std::cin.getline(str, 29);
+    std::cin.ignore(1, '\n'); // ignore previous '\n' character
+    std::cin.getline(str, sizeof(str));
 
-    for (char* p = str; *p != '\0'; p++) {
-        std::cout << *p << " ";
+    // for (char* p = str; *p != '\0'; p++) {
+    //     std::cout << *p << " ";
+    // }
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        std::cout << str[i] << " ";
     }
     return 0;
 }
