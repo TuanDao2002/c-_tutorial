bfd4079b4f01584e1f0c496358a2066015432451
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-21 18:11:31 +0700

    update

--- week4/task2.cpp
+++ week4/task2.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string.h>
 
+// using string array
+#if 0
 int main() {
     std::string str1, str2;
 
     std::cout << "Enter str1: ";
-    std::getline(std::cin, str1); // get a whole line with spaces
+    std::getline(std::cin, str1); // get a whole line with spaces for string c++ with getline
 
     int str1_len = strlen(str1.c_str()); // strlen can only apply to const char array => convert string to const char array with c_str()
     for (int i = 0; i < str1_len / 2; i++) {
@@ -31,3 +33,39 @@ int main() {
     std::cout << "String str2 after modification: " << str2 << std::endl;
     return 0;
 }
+#endif
+
+// using char array
+#if 1
+int main() {
+    char str1[100];
+    char str2[100];
+
+    std::cout << "Enter str1: ";
+    std::cin.getline(str1, 99); // get a whole line with spaces for char array using cin.getline
+
+    int str1_len = strlen(str1); // do not need to convert to char array using c_str()
+    for (int i = 0; i < str1_len / 2; i++) {
+        char temp = str1[i];
+        str1[i] = str1[str1_len - i - 1];
+        str1[str1_len - i - 1] = temp;
+    }
+
+    std::cout << "String str1 after reverse: " << str1 << std::endl;
+
+    std::cout << "Enter str2: ";
+    std::cin.getline(str2, 99);
+
+    int str2_len = strlen(str2);
+    for (int i = 0; i < str2_len; i++) {
+        if (isupper(str2[i])) {
+            str2[i] = tolower(str2[i]);
+        } else if (islower(str2[i])) {
+            str2[i] = toupper(str2[i]);
+        }
+    }
+
+    std::cout << "String str2 after modification: " << str2 << std::endl;
+    return 0;
+}
+#endif
