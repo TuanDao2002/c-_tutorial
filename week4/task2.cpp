2f9b2cea399757a97c2627b37b0a50352f3baa4b
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-23 12:03:19 +0700

    update

--- week4/task2.cpp
+++ week4/task2.cpp
@@ -9,7 +9,7 @@ int main() {
     std::cout << "Enter str1: ";
     std::getline(std::cin, str1); // get a whole line with spaces for string c++ with getline
 
-    int str1_len = str1.size(); // strlen can only apply to const char array => convert string to const char array with c_str()
+    int str1_len = str1.length(); // strlen can only apply to const char array => convert string to const char array with c_str()
     for (int i = 0; i < str1_len / 2; i++) {
         char temp = str1[i];
         str1[i] = str1[str1_len - i - 1];
@@ -21,7 +21,7 @@ int main() {
     std::cout << "Enter str2: ";
     std::getline(std::cin, str2);
 
-    int str2_len = str2.size();
+    int str2_len = str2.length();
     for (int i = 0; i < str2_len; i++) {
         if (isupper(str2[i])) {
             str2[i] = tolower(str2[i]);
