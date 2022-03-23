3606a5ee1886c20f6e6ec9c61978c037ddfdfc9f
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-23 11:17:49 +0700

    use size instead

--- week4/task2.cpp
+++ week4/task2.cpp
@@ -2,14 +2,14 @@
 #include <string.h>
 
 // using string c++
-#if 0
+#if 1
 int main() {
     std::string str1, str2;
 
     std::cout << "Enter str1: ";
     std::getline(std::cin, str1); // get a whole line with spaces for string c++ with getline
 
-    int str1_len = strlen(str1.c_str()); // strlen can only apply to const char array => convert string to const char array with c_str()
+    int str1_len = str1.size(); // strlen can only apply to const char array => convert string to const char array with c_str()
     for (int i = 0; i < str1_len / 2; i++) {
         char temp = str1[i];
         str1[i] = str1[str1_len - i - 1];
@@ -21,7 +21,7 @@ int main() {
     std::cout << "Enter str2: ";
     std::getline(std::cin, str2);
 
-    int str2_len = strlen(str2.c_str());
+    int str2_len = str2.size();
     for (int i = 0; i < str2_len; i++) {
         if (isupper(str2[i])) {
             str2[i] = tolower(str2[i]);
@@ -36,7 +36,7 @@ int main() {
 #endif
 
 // using char array
-#if 1
+#if 0
 int main() {
     char str1[100];
     char str2[100];
