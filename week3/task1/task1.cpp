91a4db9e34febedf746a54f0edf95687e1da2885
TuanDao2002 <s3877347@rmit.edu.vn>
2022-04-03 15:54:18 +0700

    update

--- week3/task1/task1.cpp
+++ week3/task1/task1.cpp
@@ -26,7 +26,7 @@ int main(){
             std::getline(std::cin, password); // use getline not cin.getline
 
             myfile << password << "\n";
-            std::cout << "Saved to the file" << "\n";
+            std::cout << "Saved to the file!" << "\n";
 
             myfile.close();
             break;
@@ -35,18 +35,18 @@ int main(){
             myfile.open("pwd.dat", std::ios::in);
 
             if (!myfile) {
-                std::cout << "Cannot open file" << "\n";
+                std::cout << "Cannot open file!" << "\n";
                 return -1;
             }
 
-            myfile >> password;
+            std::getline(myfile, password); // read the whole line from "pwd.dat" with getline => replace std::cin with fstream
             std::cout << "Your password is: " << password << "\n";
             myfile.close();
 
             break;
         
         default:
-            std::cout << "Invalid option" << "\n";
+            std::cout << "Invalid option!" << "\n";
     } 
     return 0;
 }
