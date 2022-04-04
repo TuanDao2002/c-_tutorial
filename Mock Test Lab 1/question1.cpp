aa4188de3700bae6401e0597c115ae269f244a27
TuanDao2002 <s3877347@rmit.edu.vn>
2022-04-04 21:51:04 +0700

    update to return double

--- Mock Test Lab 1/question1.cpp
+++ Mock Test Lab 1/question1.cpp
@@ -15,30 +15,6 @@
 #include <iomanip>
 #define ARG_NUM 1
 
-// doubleVal version that takes integer version
-int doubleVal(int integer) {
-    return integer * 2;
-}
-
-
-// doubleVal version that takes double version
-double doubleVal(double double_num) {
-    return double_num * 2;
-}
-
-// doubleVal version that takes hexadecimal string version
-int doubleVal(std::string hex_num) {
-    std::stringstream ss;
-    int integer_value;
-
-    ss << std::hex << hex_num;
-    ss >> integer_value;
-
-    integer_value *= 2;
-
-    return integer_value;
-}
-
 // fuction to check if a string is integer
 bool is_integer(const char* int_str) {
     int len = std::string(int_str).length();
@@ -69,6 +45,31 @@ bool is_double(const char* float_str) {
     return dot_flag;
 }
 
+// doubleVal version that takes integer version
+double doubleVal(int integer) {
+    return integer * 2;
+}
+
+
+// doubleVal version that takes double version
+double doubleVal(double double_num) {
+    return double_num * 2;
+}
+
+// doubleVal version that takes hexadecimal string version
+double doubleVal(std::string hex_num) {
+    std::stringstream ss;
+    int value;
+
+    ss << std::hex << hex_num;
+    ss >> value;
+
+    value *= 2;
+    return value;
+}
+
+
+
 int main(int argc, const char* argv[]){
     // print out warning if the number of arguments is not 1
     if (argc != ARG_NUM + 1) {
@@ -81,8 +82,8 @@ int main(int argc, const char* argv[]){
     } else if (is_double(argv[1])){ // if the string is double, use the second version of doubleVal
         std::cout << std::fixed << std::setprecision(2) << doubleVal(atof(argv[1])) << std::endl;
     } else { // otherwise, use the third version of doubleVal
-        std::cout << std::hex << std::showbase << doubleVal(argv[1]) << std::endl;
+        std::cout << std::hex << std::showbase << int(doubleVal(argv[1])) << std::endl;
     }
-    
+
     return 0;
 }
