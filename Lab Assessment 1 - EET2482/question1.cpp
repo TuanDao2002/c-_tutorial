fb44c1e35e780ea130def9fb9e38176118547069
TuanDao2002 <s3877347@rmit.edu.vn>
2022-04-08 22:26:54 +0700

    lab test 1

---
+++ Lab Assessment 1 - EET2482/question1.cpp
@@ -0,0 +1,21 @@
+/*
+    RMIT University Vietnam
+    Course: EEET2482 Software Engineering Design
+    Semester: 2022A
+    Lab Assessment: 1
+    Author: Dao Kha Tuan
+    ID: s3877347
+    Compiler used: gcc 8.1.0
+    Created  date: 09/04/2022
+    Acknowledgement: None
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+int main(){
+
+    return 0;
+}
