22e0b11004ebe29d686f9bff9ace6fcb9738f193
TuanDao2002 <s3877347@rmit.edu.vn>
2022-04-09 11:29:59 +0700

    update

--- Lab Assessment 1 - EET2482/question1.cpp
+++ Lab Assessment 1 - EET2482/question1.cpp
@@ -12,10 +12,84 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
 
-int main(){
+int main(int argc, char* argv[]){
+    // question 1.1
+    if (argc < 2) { // if there is no argument, print error
+        std::cerr << "No arguments in the command line!" << std::endl;
+        return -1;
+    }
+
+    if (strcmp(argv[1], "-w") != 0 && strcmp(argv[1], "-r") != 0) {
+        if (argc != 4) { // if there is not enough 3 hexadecimal numbers, print error
+            std::cerr << "There must be 3 hexadecimal numbers in the command line!" << std::endl;
+            return -2;
+        }
+
+        int hexSum;
+        for (int i = 1; i <= 3; i++) {
+            // use stringstream to convert hexa string to int value and add to sum
+            std::stringstream sstr;
+            sstr << std::hex << argv[i];
+            int tempVal;
+            sstr >> tempVal;
+            
+            hexSum += tempVal;
+        }
+
+        std::cout << "Sum is: " << std::hex << std::showbase << hexSum << std::endl;
+    }
+
+    // question 1.2
+    if (strcmp(argv[1], "-w") == 0) {
+        std::fstream myFile;
+        
+        // clear old content with std::ios::trunc
+        myFile.open("data.txt", std::ios::out | std::ios::trunc);
+        if (!myFile) {
+            std::cerr << "Cannot open file!!" << std::endl;
+            return -3;
+        }
+
+        // write all arguments to file
+        for (int i = 2; i < argc; i++) {
+            myFile << argv[i] << std::endl;
+        }
+        
+        myFile.close();
+
+    }
+
+    // question 1.3
+    if (strcmp(argv[1], "-r") == 0) {
+        std::fstream myFile;
+
+        // read file
+        myFile.open("data.txt", std::ios::in);
+        if (!myFile) {
+            std::cerr << "Cannot open file!!" << std::endl;
+            return -3;
+        }
+
+        int maxLength = 0;
+        std::string tempStr;
+        std::string longestLine;
+        while (std::getline(myFile, tempStr)) {
+            if (tempStr.length() > maxLength) {
+                // if the length of string is greater than max length, assign its length to max length, and itself to longest line
+                maxLength = tempStr.length();
+                longestLine = tempStr;
+            }
+        }
+
+        std::cout << "Longest line: " << longestLine << std::endl;
+        
+        myFile.close();
+    }
 
     return 0;
 }
--- Lab Assessment 1 - EET2482/question2.cpp
+++ Lab Assessment 1 - EET2482/question2.cpp
@@ -16,9 +16,240 @@
 #include <sstream>
 #include <fstream>
 
-#define STRSIZE 100;
+#define STRSIZE 100
+
+class EWallet{
+    private:
+        std::string id;
+        double balance;
+    public:
+        // constructor for EWallet
+        EWallet(){};
+
+        EWallet(std::string id, double balance) {
+            this->id = id;
+            this->balance = balance;
+        }
+
+        // getters
+        std::string getID(){
+            return id;
+        }
+
+        double getBalance(){
+            return balance;
+        }
+
+        // deposit method to add amount of money to balance
+        void deposit(double num) {
+            this->balance += num;
+            std::cout << id <<  " deposit: " << num << std::endl;
+        }
+
+        // withdraw method to deduct amount of money to balance
+        bool withdraw(double num) {
+            if (this->balance < num) { // print error if the balance less than amount of money
+                std::cerr << id <<  " cannot withdraw: " << num << std::endl;
+                return false;
+            }
+
+            this->balance -= num;
+            std::cout << id << " witdraw: " << num << std::endl;
+            return true;
+        }
+};
+
+class Customer{
+    private:
+        std::string name;
+        std::string pwd;
+        EWallet ew;
+    
+    public:
+        // constructor for Customer
+        Customer(){};
+
+        Customer(std::string name, std::string pwd, EWallet ew){
+            this->name = name;
+            this->pwd = pwd;
+            this->ew = ew;
+        };
+
+        EWallet getWallet() {
+            return ew;
+        }
+
+        void setWallet(EWallet ew) {
+            this->ew = ew;
+        }
+
+        // function to verify the password
+        bool verifyPass() {
+            std::cout << "Enter a password for " << name << " :" << std::endl;
+            char tempStr[STRSIZE];
+
+            do {
+                std::cin.getline(tempStr, sizeof(tempStr));
+            } while (strlen(tempStr) == 0);
+
+            if (tempStr != pwd) {
+                return false;
+            }
+
+            return true;
+        }
+
+        // function to deposit money of customer
+        bool doDeposit() {
+            if (!verifyPass()) {
+                std::cout << "Wrong password!" << std::endl;
+                return false;
+            }
+
+            std::cout << "Enter the amount of money to deposit: " << std::endl;
+            double depositAmount;
+            std::cin >> depositAmount;
+
+            ew.deposit(depositAmount);
+            std::cout << name << " deposits successfully!!" << std::endl;
+            return true;
+        }
+        // function to deposit money of customer
+        bool doWithdraw() {
+            if (!verifyPass()) {
+                std::cout << "Wrong password!" << std::endl;
+                return false;
+            }
+
+            std::cout << "Enter the amount of money to withdraw: " << std::endl;
+            double withdrawAmount;
+            std::cin >> withdrawAmount;
+
+            if (!ew.withdraw(withdrawAmount)) {
+                std::cout << name << " does not withdraw successfully!!" << std::endl;
+                return false;
+            }
+
+            std::cout << name << " withdraw successfully!!" << std::endl;
+            return true;
+        }
+
+        void operator >> (Customer &c) {
+            double thisBalance = this->ew.getBalance();
+            this->ew.withdraw(thisBalance);
+            EWallet newEW = c.getWallet();
+            newEW.deposit(thisBalance);
+
+            c.setWallet(newEW);
+            std::cout << c.toString() << std::endl;
+        }
+
+        Customer operator++ (int) {
+            Customer temp = *this;
+            ew.deposit(1000);
+            return temp;
+        }
+
+        std::string toString() {
+            std::stringstream sstr;
+            sstr << "Name: " << name
+                 << ", Password: " << pwd 
+                 << ", Wallet ID: " << ew.getID() 
+                 << ", Balance: " << ew.getBalance() << std::endl;
+            return sstr.str();
+        }
+};
 
 int main(){
+    // question a
+    std::cout << "Question a\n";
+    // initialize a EWallet object using dynamic allocation
+    EWallet *e = new EWallet("123", 1000);
 
+    // deposit 100 from wallet e
+    e->deposit(100);
+
+    // withdraw 1000 from wallet e
+    e->withdraw(1000);
+
+    // withdraw 200 from wallet e
+    e->withdraw(200);
+
+    delete e; 
+
+
+    // question b
+    std::cout << "\nQuestion b\n";
+    // initialize and create 4 Customer objects along with 4 EWallet objects
+    EWallet e1 = EWallet("234", 100);
+    Customer c1("tuan", "123", e1);
+
+    EWallet e2 = EWallet("235", 200);
+    Customer c2("dao", "122", e2);
+
+    EWallet e3 = EWallet("236", 310);
+    Customer c3("kha", "124", e3);
+
+    EWallet e4 = EWallet("237", 280);
+    Customer c4("khatun", "125", e4);
+
+    // deposit balance for Customer c1
+    std::cout << "Deposit balance for Customer c1: " << std::endl;
+    c1.doDeposit();
+
+    // withdraw balance for Customer c2
+    std::cout << "\nWithdraw balance for Customer c2:" << std::endl;
+    c2.doWithdraw();
+
+    // Move balance of c1 to c2
+    std::cout << "\nMove balance of c1 to c2: " << std::endl;
+    c1 >> c2;
+    std::cout << "Account of Customer c2: " << c2.getWallet().getBalance() << std::endl;
+    std::cout << "Account of Customer c1: " << c1.getWallet().getBalance() << std::endl;
+
+    // test ++ postfix on c4
+    Customer tempCustomer = c4++;
+    std::cout << "Info of Customer c4++: " << std::endl;
+    std::cout << tempCustomer.toString() << std::endl;
+    std::cout << "Info of Customer c4:" << std::endl;
+    std::cout << c4.toString() << std::endl;
+
+    // a dynamic array of 4 customers
+    Customer *customerArr = new Customer[4]{c1, c2, c3, c4};
+
+    // open file with std::ios::trunct to delete old content and write new one
+    std::fstream infile;
+    infile.open("customer.txt", std::ios::out | std::ios::trunc);
+
+    if (!infile) {
+        std::cerr << "Cannot open file!!!" << std::endl;
+        return -1;
+    }
+
+    Customer largestBalanceCustomer = c1;
+    Customer secondLargestBalanceCustomer = c1;
+
+    std::cout << "\nInfo of all 4 customers: " << std::endl;
+    for (int i = 0; i < 4; i++) {
+        std::cout << customerArr[i].toString() << std::endl;
+        infile << customerArr[i].toString();
+
+        // if balance of customer is larger than one with largest balance
+        if (customerArr[i].getWallet().getBalance() >= largestBalanceCustomer.getWallet().getBalance()) {
+            secondLargestBalanceCustomer = largestBalanceCustomer; // swap the second largest and largest before
+            largestBalanceCustomer = customerArr[i]; // assign the customer to one with largest balance
+        } else if (customerArr[i].getWallet().getBalance() > secondLargestBalanceCustomer.getWallet().getBalance()) {
+            // assign the customer to one with second largest balance
+            secondLargestBalanceCustomer = customerArr[i];
+        } 
+    
+    }
+
+    std::cout << "Info of customer with the second largest balance in the wallet: " << std::endl;
+    std::cout << secondLargestBalanceCustomer.toString() << std::endl;
+
+    infile.close();
+
+    delete[] customerArr;
     return 0;
 }
